235636d3038f75a72b1faaafbb76ac31a5a0bdc4
minty99 <mhkim4886@gmail.com>
2019-03-21 11:46:39 +0900

    Solved 3053

---
+++ 03000/03053/03053.cpp14.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tgmath.h>
+#include <tuple>
+#include <unistd.h>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef pair<lli, lli> plli;
+typedef unsigned char byte;
+typedef unsigned int uint;
+typedef unsigned long long int ulli;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int r;
+    scanf("%d", &r);
+
+    printf("%f\n", M_PI * r * r);
+    printf("%d\n", r * r * 2);
+}
